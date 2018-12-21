b56b250a7aa11526dd604d1a83f369a4f0cf0a46
Yoni <Yoni@Yoni-Dell>
2018-12-21 11:42:26 +0200

    First commit

---
+++ Stam/src/Main.java
@@ -0,0 +1,50 @@
+
+
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Variables
+		// ==========
+		
+		// Numbers
+		int Number = 3;
+		int Value;
+		Value = 2;
+		Value = Number;
+		double digits = 2.5;
+		short digit = 1;
+		
+		// Strings
+		char letter = 'a';
+		String name = "yoni";
+		String newName = name;
+		
+		// Arrays
+		String[] names = {"yoni", "Amit", "Avi"};
+		names[0] = "Yoni";
+		names[0] = null;
+		char[] letters = {'h', 'e', 'l', 'l', 'o'};
+		
+		// IO
+		System.out.println(names[1]);
+		System.out.println(digits);
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("enter an integer");
+		int myint = keyboard.nextInt();
+		System.out.println(myint);
+		
+		final int constant = 3;
+		
+		
+		// Operators
+		
+		
+	}
+
+}
