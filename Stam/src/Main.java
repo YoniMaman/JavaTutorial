56827c754ad66aa0c9fa5fad314563e1bd0801a2
Yoni <Yoni@Yoni-Dell>
2018-12-29 15:47:35 +0200

    Added file reading

--- Stam/src/Main.java
+++ Stam/src/Main.java
@@ -1,97 +1,34 @@
 
 
 import java.util.Scanner;
+import java.io.*;
 
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException  {
 		// TODO Auto-generated method stub
 		
-		// Variables
-		// ==========
+		//Map content = new HashMap();
+		final String payCheck = "משכורת";
+
+		String file = "C:\\Users\\Yoni\\Downloads\\FibiSave20181227283265.csv";
+		try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+	        String line = "";
+	        while ((line = br.readLine()) != null) {
+	            
+	        	if ((line.split(",")[2].replace("\"", " ").trim()).equals(payCheck))
+	        	{
+	        		String currValue = line.split(",")[4];
+		        	System.out.println(currValue);
+	        	}
+	        	
+	        }
+	    } catch (FileNotFoundException e) {
+	      //Some error logging
+	    }
+		    }
 		
-		// Numbers
-		int Number = 3;
-		int Value;
-		Value = 2;
-		Value = Number;
-		double digits = 2.5;
-		short digit = 1;
-		
-		
-		// Strings
-		char letter = 'a';
-		String name = "yoni";
-		String newName = name;
-		
-		// Boolean
-		boolean isTrue = true;
-		boolean isFalse = false;
-		isTrue = isFalse;
-		
-		// Arrays
-		String[] names = {"yoni", "Amit", "Avi"};
-		names[0] = "Yoni";
-		names[0] = null;
-		char[] letters = {'h', 'e', 'l', 'l', 'o'};
-		
-		// IO
-		System.out.println(names[1]);
-		System.out.println(digits);
-		
-		Scanner keyboard = new Scanner(System.in);
-		System.out.println("enter an integer");
-		int myint = keyboard.nextInt();
-		System.out.println(myint);
-		
-		final int constant = 3;
-		
-		
-		// Operators
-		// =========
-		
-		// Numbers
-		int first = 2;
-		int second = 3;
-		int sum = first + second;
-		
-		int sumAgain = 0;
-		double aDouble = 3.4;
-		// sumAgain = aDouble + first; // Can't do that
-		
-		int div = second / first;
-		int mod = second % first;
-		int multi = second * first;
-		int sub = second - first;
-		
-		second++;
-		first--;
-		second += Value;
-		
-		// Strings
-		String hello = "hello";
-		String Goodbye = "Goodbye";
-		String helloGoodbye = hello + Goodbye;
-		helloGoodbye += " and happy birthday";
-		
-		// Conditions
-		// ==========
-		
-		int smallNum = 0;
-		int bigNum = 30;
-		
-		if (smallNum < bigNum)
-		{
-			System.out.println(smallNum + " is smaller");
-		}
-		else
-		{
-			System.out.println((smallNum + " is not smaller"));
-		}
-		
-		
-		
-	}
+	
 
 }
